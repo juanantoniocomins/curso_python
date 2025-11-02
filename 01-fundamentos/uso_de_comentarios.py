e092a02e64dd9684af7af93c6a6f0baba34f1d0f
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-02 23:59:09 +0100

    Add files via upload

---
+++ 01-fundamentos/uso_de_comentarios.py
@@ -0,0 +1,13 @@
+# Esto es un comentario de línea completa.
+x = 10  # Esto es un comentario después de una instrucción de código.
+
+"""
+Este es un ejemplo de un 'docstring'
+que abarca varias líneas.
+Se utiliza comúnmente para documentar
+funciones, clases y módulos completos.
+"""
+
+'''
+Tb se puede utilizar con millas simples
+'''
