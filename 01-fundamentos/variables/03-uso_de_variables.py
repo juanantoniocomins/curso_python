fdb7a70dfcef7b89fa86d6e06c5c3d59616c90e9
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-02 23:57:49 +0100

    Add files via upload

---
+++ 01-fundamentos/variables/03-uso_de_variables.py
@@ -0,0 +1,7 @@
+nombre = "Pedro"
+edad = 33
+altura = 1.75
+es_estudiante = True
+
+# Usando comas para separar los elementos
+print("Nombre:", nombre, "- Edad:", edad, "- Altura:", altura, "- Estudiante:", es_estudiante)
