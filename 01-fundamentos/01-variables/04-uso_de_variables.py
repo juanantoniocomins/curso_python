41756054181111e8893779e4250d4305470c2626
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-03 00:27:53 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/04-uso_de_variables.py
@@ -0,0 +1,7 @@
+nombre = "Pablo"
+edad = 33
+altura = 1.75
+es_estudiante = True
+
+# Usando comas para separar los elementos + salto de linea
+print("Nombre:", nombre, "\nEdad:", edad, "\nAltura:", altura, "\nEstudiante:", es_estudiante)
