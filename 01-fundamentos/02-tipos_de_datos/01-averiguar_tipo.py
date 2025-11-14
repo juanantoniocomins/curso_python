e072a5e822f6efd040b0a17adc67717333a7a363
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:38:44 +0100

    Delete 01-fundamentos/02-tipos_de_datos/01-averiguar_tipo.py

--- 01-fundamentos/02-tipos_de_datos/01-averiguar_tipo.py
+++
@@ -1,9 +0,0 @@
-edad = 33
-altura = 1.75
-es_estudiante = True
-nombre = "Pablo"
-
-print(f"edad = {type(edad)}")
-print(f"altura = {type(altura)}")
-print(f"es_estudiante = {type(es_estudiante)}")
-print(f"nombre = {type(nombre)}")
