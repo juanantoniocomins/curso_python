1087e73a7fc38f3866c5c81f4892f6d60a0ec8ba
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:35:20 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/ejercicios/04-de_fahrenheit_a_celsius.py
@@ -0,0 +1,10 @@
+# Convierte una temperatura en grados Fahrenheit a Celsius a
+# F = (C x 1.8) + 32
+
+# C = (F - 32) / 1.8
+
+fahrenheit = 98.96
+celsius = (98.96-32) / 1.8
+
+
+print(f"{fahrenheit:.2f}ºF = {celsius:.2f}ºC")
