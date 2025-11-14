1de8fb21c622b805bb327b68ee68b9f763c6b011
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:41:14 +0100

    Add files via upload

---
+++ 01-fundamentos/02-tipos_de_datos/03_valor_none.py
@@ -0,0 +1,13 @@
+# None es un valor especial en Python que representa la ausencia de valor.
+# NO es lo mismo que 0, una cadena vacía (""), o False.
+# Su tipo de dato es NoneType.
+
+## 1. Declaración e Inicialización
+
+# Puedes inicializar una variable a None para indicar que aún no tiene un valor útil.
+valor_pendiente = None
+print(f"1. Valor de la variable inicializada: {valor_pendiente}")
+
+# Verifica el tipo de dato:
+print(f"2. Tipo de 'valor_pendiente': {type(valor_pendiente)}")
+print("-" * 30)
