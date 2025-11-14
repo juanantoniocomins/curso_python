ddc2da394dcb45efe082cec729947ad8aaabaa9f
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:35:53 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/ejercicios/06-concatenacion_de_cadenas.py
@@ -0,0 +1,9 @@
+# Combina nombre y apellido en una sola cadena.
+
+nombre = "Ana"
+apellido1 = "Peréz"
+apellido2 = "Sánchez"
+
+nombre_completo = nombre + " " + apellido1 + " " + apellido2 + "."
+
+print(nombre_completo)
