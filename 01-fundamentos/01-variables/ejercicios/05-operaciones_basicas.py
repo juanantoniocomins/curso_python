14db4d58833f892b2826b3a87c2078c65a04bc50
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:35:35 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/ejercicios/05-operaciones_basicas.py
@@ -0,0 +1,12 @@
+# Realiza operaciones básicas entre dos números.
+
+numero1 = 12
+numero2 = 6
+
+print(f"{numero1} + {numero2} = {numero1+numero2}")
+print(f"{numero1} - {numero2} = {numero1-numero2}")
+print(f"{numero1} x {numero2} = {numero1*numero2}")
+print(f"{numero1} / {numero2} = {numero1/numero2}")
+print(f"{numero1} // {numero2} = {numero1 // numero2}") # división entera
+print(f"{numero1} % {numero2} = {numero1%numero2}") # modulo, resto división
+print(f"{numero1} ** {numero2} = {numero1**numero2}") # potencia
