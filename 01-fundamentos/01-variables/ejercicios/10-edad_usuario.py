dce8a1c2b265b904606f2423200aed447bc11524
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:36:40 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/ejercicios/10-edad_usuario.py
@@ -0,0 +1,7 @@
+# a través del año de nacimiento, calcula la edad actual.
+
+anyo_nacimiento = 1972
+any_actual = 2025
+edad_actual = 2025 - 1972
+
+print(f"La edad actual es de {edad_actual} años.")
