fc2ea1ae37fbb7d1ed71752c2b55e6f713d7cc6b
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-14 23:34:32 +0100

    Add files via upload

---
+++ 01-fundamentos/01-variables/ejercicios/01-intercambio_valores.py
@@ -0,0 +1,13 @@
+# Intercambiar los valores de dos variables sin usar una variable auxiliar.
+
+var1 = 13
+var2 = 26
+
+print("Antes: var1 = " + str(var1) + ", var2 = " + str(var2))
+
+var1, var2 = 26, 13
+
+print("Antes: var1 = " + str(var1) + ", var2 = " + str(var2))
+
+
+
