2b4c76d6325987fa4469d45094827283f193513e
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-19 23:08:25 +0100

    Add files via upload

---
+++ 01-fundamentos/03-operadores/ejercicios/01-aritmeticos/04-moduo.py
@@ -0,0 +1,8 @@
+# obtener el resto de una divison
+
+a = 43
+b = 5
+resultado = a % b
+
+print (f"El resto de dividir {a} entre {b} = {resultado}")
+print (f"Porque {a}/{b} = {a//b} - {((a//b)-resultado)} = {resultado}")
