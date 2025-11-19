46dd74662961f750758fdb07d312448b6cdbe4c1
Juan Antonio Comíns Tormo <89415337+juanantoniocomins@users.noreply.github.com>
2025-11-19 23:07:50 +0100

    Add files via upload

---
+++ 01-fundamentos/03-operadores/ejercicios/01-aritmeticos/02-division.py
@@ -0,0 +1,10 @@
+# muestra decimales
+
+dividento = 120
+divisor = 5
+
+print(f"{dividento} / {divisor} = {dividento/divisor}")
+
+divisor2 = 7
+
+print(f"{dividento} / {divisor2} = {dividento/divisor2}")
